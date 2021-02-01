b5251c713ca9e2c2ae3f71975c6b977cb205a8d5
Daniel Reina Ramos <drrmovil@gmail.com>
2021-02-01 18:37:25 +0100

    añadidos paises randoms y sus capitales funcionalmente

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,80 @@
-import React, { Component } from "react";
-import countries from "../countries.json";
+import React, { useState } from "react";
+import allCountries from "../countries.json";
 
+const RandomCountry = () => {
+  const [countries, setCountries] = useState(allCountries);
+  const [randomC, setRamdonC] = useState(false);
+  const [capital, setCapital] = useState(false)
+  const [verify, setVerify] = useState(false)
+  const [submit, setSubmit] = useState(false);
 
-class RandomCountry extends Component {
-  constructor() {
-    super();
-    this.state = {
-     countries:false
-    };
+  function eliminarDiacriticosEs(texto) {
+    return texto
+      .normalize("NFD")
+      .replace(
+        /([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+        "$1"
+      )
+      .normalize();
   }
 
-  componentDidMount() {
-     this.setState({countries: [...countries]})
-  }
+  const getRandomCountry = (e) => {
+    e.preventDefault();
+    let ctries = [...allCountries];
+    let randomC = ctries[Math.floor(Math.random() * ctries.length)];
+    setRamdonC(randomC);
+     setVerify(false);
+  };
+  const confirm = (e) => {
+    e.preventDefault();
+    if (capital) {
+      if (capital.toLowerCase() === randomC.capitalEsp[0].toLowerCase()) {
+        setVerify(true);
+      } else { setVerify(false); }
+    }
+    setSubmit(true);
+    console.log(countries[9].capitalEsp);
+  };
 
-  render() {
-    console.log("holaaa", this.state.countries);
-    return (
-      <div>
-        {this.state.countries ? (
-          <>
-            <h1>hola locoo</h1>
-          </>
-        ) : null}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {countries && (
+        <>
+          <form onSubmit={getRandomCountry}>
+            <input
+              type="submit"
+              className="button-primary u-full-width"
+              value="Nuevo país aleatorio"
+            />
+          </form>
+          {randomC &&
+            <><h1>{randomC.translations.spa.common}</h1>
+          <form onSubmit={confirm}>
+            <input
+              type="text"
+              placeholder="Capital"
+              onChange={(e) => setCapital(e.target.value)}
+            ></input>
+            <input
+              type="submit"
+              className="button-primary u-full-width"
+              value="Probar suerte"
+            />
+          </form>
+          {submit && (
+            <>
+              {verify ? (
+                <p>Respuesta correcta, que sabia eres Goyi!</p>
+              ) : (
+                <p>Incorrecto, prueba de nuevo abuela ¡Venga que tu puedes!</p>
+              )}
+            </>
+          )}
+          </>}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default RandomCountry;
